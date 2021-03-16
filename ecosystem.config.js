a30a4cab641c1f21a56b8d5e5f1126e7ae1c85bb
Leandro <leandrofernandesvieira@gmail.com>
2021-03-15 23:17:35 -0300

    chore: setup heroku with github actions

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "moderacompra-backend",
+    script: "./src/server.js",
+    instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 import app from './src/app.js';
 
 app.listen(3000, () => {
