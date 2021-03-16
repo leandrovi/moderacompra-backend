a2be69279aca423f219bc6aa786ff3386bb5ce31
Leandro <leandrofernandesvieira@gmail.com>
2021-03-15 23:49:09 -0300

    fix: use env port

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@
 import app from './app.js';
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('🚀 ModeraCompra server listening on port 3000!')
+  console.log('🚀 ModeraCompra server is running!')
 });
