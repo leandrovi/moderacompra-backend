cc547ba1b2d336a0ea0c4e35e403e43f5cea0196
Leandro <leandrofernandesvieira@gmail.com>
2021-03-20 11:36:40 -0300

    chore: adjust deploy scripts to use tsc

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req: Request, res: Response) =>
   res.status(200).json({ message: "Hello World!" })
 );
