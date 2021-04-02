d6ca13b8817c8fc85a5a12b421090691c7dfdba4
Leandro <leandrofernandesvieira@gmail.com>
2021-04-01 23:04:49 -0300

    fix: prevent module.exports on database instance

--- src/database/postgres/config/index.ts
+++ src/database/postgres/config/index.ts
@@ -49,10 +49,8 @@ module.exports = {
   username,
   password,
   database,
-  operatorAliases: false,
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
   },
 };
--- src/database/postgres/index.ts
+++ src/database/postgres/index.ts
@@ -1,7 +1,45 @@
 import { Sequelize } from "sequelize";
-import path from 'path';
 
-const config = require(path.resolve(__dirname, 'config', 'index.ts'));
+// Need to repeat this logic in order to prevent bugs from module.exports
+const databaseCredentials = {
+  development: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialectOptions: {
+      ssl: false
+    }
+  },
+  test: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+  production: {
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  },
+};
+
+const { host, username, password, database, dialectOptions } = databaseCredentials[
+  process.env.NODE_ENV
+];
 
 export default class Database {
   private static instance: Database;
@@ -12,7 +50,20 @@ export default class Database {
   }
 
   private init(): void {
-    this.connection = new Sequelize(config);
+    this.connection = new Sequelize(
+      database,
+      username,
+      password,
+      {
+        dialect,
+        dialectOptions,
+        host,
+        define: {
+          timestamps: true,
+          underscored: true,
+        }
+      }
+    );
   }
 
   public static getInstance(): Database {
