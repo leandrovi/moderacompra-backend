b039cc631ec5e77d95c1a7b7806e70e404d7d248
Leandro <leandrofernandesvieira@gmail.com>
2021-05-01 22:48:11 -0300

    fix: adjust PK on product

--- src/database/postgres/migrations/20210502002529-add-name-to-products.js
+++ src/database/postgres/migrations/20210502002529-add-name-to-products.js
@@ -4,14 +4,12 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.changeColumn('products', 'name', {
       type: Sequelize.STRING,
-      unique: true,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     return queryInterface.changeColumn('products', 'name', {
       type: Sequelize.STRING,
-      unique: false
     });
   }
 };
